26a7b8781a44f59f70202c8372352d835a8d61a4
Denchik1985 <Denis85gilmanov@gmail.com>
2026-03-02 19:58:47 +0500

    создание угадай_номер

--- bin/dart_tutor.dart
+++ bin/dart_tutor.dart
@@ -1,16 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
+import 'dart:math';
 
 void main() {
-  print('Как тебя зовут, Путник?');
-
-  String? name = stdin.readLineSync(encoding: utf8);
-
-  if (name != null && name.isNotEmpty) {
-    print('Привет $name! Рад познакомиться!');
-    print('Твоё имя состоит из ${name.length} букв.');
-  } else {
-    print('Эй, ну как тебя зовут-то?');
-  }
-  print('Программа завершена.');
+  
 }
---
+++ bin/guess_the_number.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:math';
+
+void main() {
+  int secret = Random().nextInt(10) + 1;
+
+  int guess = 0;
+  int attempts = 0;
+
+  print('Я загадал число от 1 до 10. Угадай!');
+
+  while (guess != secret) {
+    attempts++;
+    print('Попытка $attempts');
+
+    String? input = stdin.readLineSync(encoding: utf8);
+    if (input == null || input.isEmpty) continue;
+
+    guess = int.parse(input);
+
+    if (guess < secret){
+      print ('Больше!');
+      print ('God Mode $secret');
+    } else if (guess > secret){
+      print('Меньше!');
+      print ('God Mode $secret');
+    } else {
+      print('Поздравляю, ты угадал за $attempts попыток');
+    }
+  }
+}
