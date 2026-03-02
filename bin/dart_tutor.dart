4c274c2694df7e7ef743c325a8ab088db31c7d08
Denchik1985 <Denis85gilmanov@gmail.com>
2026-03-02 10:14:02 +0500

    добавил ввод имени пользователя

--- bin/dart_tutor.dart
+++ bin/dart_tutor.dart
@@ -1,5 +1,15 @@
-import 'package:dart_tutor/dart_tutor.dart' as dart_tutor;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_tutor.calculate()}!');
+void main() {
+  print('Как тебя зовут, Путник?');
+
+  String? name = stdin.readLineSync();
+
+  if (name != null && name.isNotEmpty) {
+    print('Привет $name! Рад познакомиться!');
+    print('Твоё имя состоит из ${name.length} букв.');
+  } else {
+    print('Эй, ну как тебя зовут-то?');
+  }
+  print('Программа завершена.');
 }
